327b182bcc94fd611ea63549ed0620059dd5e6c5
MarcoAcce <149236335+MarcoAcce@users.noreply.github.com>
2024-08-31 00:40:47 +0200

    completed the analysis

---
+++ src/Analysis.py
@@ -0,0 +1,103 @@
+from os import listdir, getcwd
+from os.path import isfile, join
+from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import SplitExcel as se
+
+
+if __name__ == '__main__':
+    analysis_directory = join(getcwd(),r'analysis')
+    analysis_input_file = join(
+        analysis_directory, f'model_predictions.xlsx')
+    #split the model_predictions 
+    try: se.SplitExcel(analysis_input_file, 0, analysis_directory)
+    except Exception as ex: 
+        print(ex)
+        quit()
+    #select all files split from model_predictions
+    #listdir returns only the files' name and not the paths
+    #the split removes the file's extension    
+    analysis_split_directory = join(analysis_directory, r'split')
+    analysis_files_names = [f.split(".xlsx")[0] 
+                            for f in listdir(analysis_split_directory) 
+                            if isfile(join(analysis_split_directory, f))]
+    #creates empty dictionaries for the results 
+    rms_list = []
+    mse_list = []
+    total_mse = 0
+    total_patient_number = 0
+    
+    #iterate through all split files
+    for file_name in analysis_files_names:
+        #reconstruct the path to each file
+        file = join(analysis_split_directory, file_name + ".xlsx")
+        #reads age and predictions for the file
+        df = pd.read_excel(file)[['AGE_AT_SCAN','PREDICTED_AGE']]
+        ages = df['AGE_AT_SCAN'].to_numpy(dtype=float)
+        predictions = df['PREDICTED_AGE'].to_numpy(dtype=float)
+        #sanity check for the arrays' dimensions
+        if ages.size != predictions.size:
+            raise Exception("Ages and predicted ages differ in number!")
+    
+        #calculate mse and rms for each group and save value to a dictionary
+        mse = 0
+        rms = 0
+    
+        for age, prediction in zip(ages,predictions):
+            square_error = (age - prediction) ** 2
+            mse += square_error
+            rms += sqrt(square_error)
+            total_mse += square_error
+        mse = mse / ages.size  
+        rms = rms / ages.size
+        rms_list.append(rms)
+        mse_list.append(mse)
+        total_patient_number += ages.size
+        print(file_name, "   rms: ", rms, "   mse: ", mse)
+    
+    total_mse = total_mse / total_patient_number
+    total_rms = sqrt(total_mse)
+    print("Complete dataset", "   rms: ", total_rms, "   mse :", total_mse)
+    
+    #create two subplots for mse and rms
+    fig, axs = plt.subplots(2, sharex=True, figsize=(14, 10))
+    rms_plot = axs[0]
+    mse_plot = axs[1]
+    
+    #rms subplot
+    rms_plot.plot(analysis_files_names,rms_list)
+    #array of all the y value for the grid
+    y_rms_ticks = np.arange(0, max(rms_list) + 1, 1)
+    #grid
+    rms_plot.set_yticks(y_rms_ticks)  
+    rms_plot.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    #horizontal line at the total value
+    rms_plot.axhline(total_rms, color='black', 
+                     linestyle=':', label=f'Complete data RMS: {total_rms:.2f}')
+    rms_plot.set(xlabel = 'Group', ylabel = 'RMS [yr]')
+    #legend
+    rms_plot.legend(loc='upper right', bbox_to_anchor=(1, 1))
+    
+    #mse subplot
+    mse_plot.plot(analysis_files_names,mse_list)
+    #array of all the y value for the grid
+    y_mse_ticks = np.arange(0, max(mse_list) + 20, 20)
+    #grid
+    mse_plot.set_yticks(y_mse_ticks)     
+    mse_plot.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+    #horizontal line at the total value
+    mse_plot.axhline(total_mse, color='black', 
+                     linestyle=':', label=f'Complete data MSE: {total_mse:.2f}')
+    mse_plot.set(xlabel = 'Group', ylabel = 'MSE')
+    #legend
+    mse_plot.legend(loc='upper right', bbox_to_anchor=(1, 1))
+    
+    #group name labels rotation
+    plt.xticks(rotation=45, ha='right')
+    #save plot as result.png
+    plt.savefig(analysis_directory + "\\result.png", dpi=500, bbox_inches='tight')
+    plt.show()
+    
--- src/SplitExcel.py
+++ src/SplitExcel.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import os
+from os import getcwd, path
 
-def SplitExcel(file_path: str, columnForSplitting: int =0):
+def SplitExcel(file_path: str, columnForSplitting: int =0,
+               output_directory_path: str = None):
     """
     Function to split an .xlsx file into multiple files, grouping together
     all rows which share the same key in a column.
@@ -13,21 +14,30 @@ def SplitExcel(file_path: str, columnForSplitting: int =0):
     columnForSplitting (int): index of the column to be used for grouping 
     (starts at 0).
     
+    output_directory_path (str): path to the directory to which the output 
+    will be printed. Default at None, will use the working directory
+    
     """
+    if not path.isfile(file_path): 
+        raise Exception("file at ",file_path, "not found!")
+
     # Load the original .xlsx file
-    df = pd.read_excel(input_file)
+    df = pd.read_excel(file_path)
     
     # Group data by the name in the first column
     #Will take only the substring before the "_" char.
     df['GroupKey'] = df[df.columns[0]].str.split('_').str[0]
     grouped = df.groupby('GroupKey')
     
+    if output_directory_path == None:
+        output_directory_path = getcwd()
+       
     # Loop through each group and write to separate .xlsx files
     for key, group in grouped:
         group = group.drop(columns=['GroupKey'])
         # Create a new filename based on the value in the first column
-        output_file = os.path.join(
-            current_directory, r'input\split', f"{key}.xlsx" )
+        output_file = path.join(
+            output_directory_path, r'split', f"{key}.xlsx" )
         
         # Save the group to a new .xlsx file, including the column titles
         group.to_excel(output_file, index=False)
@@ -36,11 +46,13 @@ def SplitExcel(file_path: str, columnForSplitting: int =0):
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
+        
+    current_directory = getcwd()
     
-    current_directory = os.getcwd()
-    input_file = os.path.join(
-        current_directory, r'input\FS_features_ABIDE_males_someGlobals.xlsx')
-    SplitExcel(input_file,0)
+    input_file = path.join(
+        current_directory,r'input\FS_features_ABIDE_males_someGlobals.xlsx')
+    
+    SplitExcel(input_file,0, path.join(current_directory, r"input"))
     
     
